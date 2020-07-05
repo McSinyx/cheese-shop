b8dbdd7540dd41f5a10c182f2ff3ebf8285d2457
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-07-05 22:40:31 +0700

    Outline report and nitpick

--- sql/ctl.sql
+++ sql/ctl.sql
@@ -8,5 +8,8 @@ IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON cheese_shop.*
 TO wensleydale@localhost;
 
-UPDATE user SET plugin='mysql_native_password' WHERE User='wensleydale';
+UPDATE user
+SET plugin='mysql_native_password'
+WHERE User='wensleydale';
+
 FLUSH PRIVILEGES;
